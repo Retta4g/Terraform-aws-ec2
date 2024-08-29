9c41a75ec6075e0154909ad9dde8c84c96683ad8
Retta4g <messiwhanna@gmail.com>
2024-08-29 13:54:07 -0500

    web

--- outputs.tf
+++
@@ -1,4 +0,0 @@
-output "public_ip" {
-  description = "The public IP address of the instance"
-  value       = aws_elastic_ip.eip.public_ip
-}
---
+++ resources.tf
@@ -0,0 +1,105 @@
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_ed25519.pub")
+}
+
+variable "prefix" {
+  type    = string
+  default = "project-aug-28"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "172.16.0.0/16"
+  tags = {
+    Name = join("-", ["${var.prefix}", "vpc"])
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "172.16.0.0/24"
+
+  tags = {
+    Name = join("-", ["${var.prefix}", "subnet"])
+  }
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
+}
+
+module "remote_module" {
+  # source = terraform-aws-security-groups-027
+  source  = "app.terraform.io/02-spring-cloud/security/aws"
+  version = "0.0.0"
+  vpc_id  = aws_vpc.main.id
+
+  security_groups = {
+    "web" = {
+      "description" = "Security Group for Web Tier"
+      "ingress_rules" = [
+        {
+          to_port     = 22
+          from_port   = 22
+          cidr_blocks = ["0.0.0.0/0"]
+          protocol    = "tcp"
+          description = "ssh ingress rule"
+        },
+        {
+          to_port     = 80
+          from_port   = 80
+          cidr_blocks = ["0.0.0.0/0"]
+          protocol    = "tcp"
+          description = "http ingress rule"
+        },
+        {
+          to_port     = 443
+          from_port   = 443
+          cidr_blocks = ["0.0.0.0/0"]
+          protocol    = "tcp"
+          description = "https ingress rule"
+        }
+      ]
+    },
+  }
+}
+
+resource "aws_instance" "server" {
+  ami           = "ami-066784287e358dad1"
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.deployer.key_name
+
+  subnet_id              = aws_subnet.main.id
+  vpc_security_group_ids = [module.remote_module.security_group_id["web"]]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo yum install -y httpd
+              sudo systemctl start httpd.service
+              sudo systemctl enable httpd.service
+              sudo echo "<h1> Hello World from BamBam </h1>" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = join("-", [var.prefix, "ec2"])
+  }
+}
+
+resource "aws_eip" "main" {
+  instance = aws_instance.server.id
+}
